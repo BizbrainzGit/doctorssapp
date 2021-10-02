38a832974b2d76d79512275ed13545aea6effb97
Your Name <keshoju.rravali@vonkdoth.com>
2021-10-02 21:53:19 +0530

    route changes

--- application/config/config.php
+++ application/config/config.php
@@ -38,7 +38,7 @@ $config['base_url'] = 'doctorssapp';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = ' ';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
